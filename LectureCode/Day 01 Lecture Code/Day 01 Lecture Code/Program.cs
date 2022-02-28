946f02b22060a6f9ec73ec16690def5cc64f48cd
Louis Fisher <LJFisher1@student.fullsail.edu>
2022-02-28 15:15:46 -0600

    PrintMessage update

--- LectureCode/Day 01 Lecture Code/Day 01 Lecture Code/Program.cs
+++ LectureCode/Day 01 Lecture Code/Day 01 Lecture Code/Program.cs
@@ -8,9 +8,22 @@ namespace Day_01_Lecture_Code
         {
             PrintMessage();
             string msg = GetMessage();
+            PrintMessage(msg);
+
+            int num1 = 10, num2 = 50;
+            int result = Add(num1, num2);
             Console.ReadKey();
         }
 
+        static int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        static void PrintMessage(string one)
+        {
+            Console.WriteLine(one);
+        }
         static void PrintMessage()
         {
             Console.WriteLine("German superheroes are the wurst!");
