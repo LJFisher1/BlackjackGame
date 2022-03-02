bddd7712cd25afdb098e7ca44850a28bb43136df
Louis Fisher <LJFisher1@student.fullsail.edu>
2022-03-02 08:05:02 -0600

    3.2.22 morning push

--- PG2Input/Input.cs
+++ PG2Input/Input.cs
@@ -8,5 +8,20 @@ namespace PG2Input
 {
     public static class Input
     {
-    }
+        public static int ReadInteger(string entry, int )
+        {
+            Console.Write("Please enter an integer: ");
+            string userInput = Console.ReadLine();
+            bool validEntry = int.TryParse(userInput, out int validInput);
+            if (validEntry)
+            {
+                Console.WriteLine("Good on you for entering a valid integer.");
+            }
+            else
+            {
+                Console.Write("Sorry, thats an invalid entry. Please try again: ");
+            }
+            return validInput;
+        }
+    }   
 }
