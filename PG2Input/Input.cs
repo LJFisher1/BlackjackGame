ceb65c8b72b94288f2f3924a03d88d88125039e5
Louis Fisher <LJFisher1@student.fullsail.edu>
2022-03-02 23:02:48 -0600

    ReadString working

--- PG2Input/Input.cs
+++ PG2Input/Input.cs
@@ -11,7 +11,7 @@ namespace PG2Input
         public static int ReadInteger(string prompt, int min, int max)
         {
             bool validInput = false;
-            int one = 0;
+            int zed = 0;
             while(validInput == false)
             {
                 Console.Write(prompt);
@@ -20,28 +20,65 @@ namespace PG2Input
                 {
                     if (validOutput > min && validOutput < max)
                     {
-                        one = validOutput;
+                        zed = validOutput;
                         break;
                     }
                     else
                     {
-                        Console.WriteLine("Input out of range, user loser");
+                        Console.WriteLine("Input out of range, try again.");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Try again..");
+                    Console.WriteLine("Thats not an integer.");
                 }
             }
-            return one;
+            return zed;
 
         }
-        static void ReadString()
+
+        //Create a method called ReadString that will ask the user for a string. Instead of returning the value like in 
+        // ReadInteger, you should use pass by reference to get the string back to the caller.The method should show a
+        //prompt, read the user’s input, store the input in the ref parameter.If the user does NOT enter a string, show an
+        //error message to them, show the prompt again and ask for the user’s input. You’ll need a loop for this. Do not
+        // return until the user enters something. You should use the IsNullOrEmpty or the IsNullOrWhiteSpace methods of 
+        // the string class to check if the string is empty
+
+
+        public static void ReadString(string prompt, ref string value)
         {
-            Console.Write("");
+            bool validString = false;
+            while (validString == false)
+            {
+                Console.Write(prompt);
+                string s = Console.ReadLine();
+                {
+                    if (string.IsNullOrEmpty(s))
+                    {
+                        Console.WriteLine("Invalid entry.");
+                    }
+                    else
+                    {
+                        value = s;
+                        break;
+                    }
+                }   
+            }
         }
-        static void ReadChoice()
+        //        Create a method called ReadChoice that will ask the user to select from a list of options, like a menu.Instead of
+        //returning like ReadInteger or passing back the value like ReadString, you should return the selection through an 
+        //out parameter.The method should show a list of options to the user, show a prompt, get the user’s selection, and 
+        //return the selection through an out parameter.
+        //You’ll need to pass the list of options as a string array.Something like string[] {“1.Add Car”, “2.Show Cars”, “3.
+        //Exit” }. The method should loop over the array and show each option on a new line.Then it should show the
+        //prompt and read the user’s input.You should use your ReadInteger method you created earlier
+        static void ReadChoice(string prompt, string[] options, out int selection)
         {
+            for(int i = 0; i < options.Length; i++)
+            {
+
+            }
+                selection = something using readint method
 
         }
     }  
